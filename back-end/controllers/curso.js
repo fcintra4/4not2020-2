74547db075dff54a899bbf1a1a011046c354a20a
fcintra4 <github4@faustocintra.com.br>
2020-09-11 01:28:58 +0000

    (10/09) Métodos RETRIVE no controller

--- back-end/controllers/curso.js
+++ back-end/controllers/curso.js
@@ -50,4 +50,34 @@ controller.novo = async (req, res) => {
     }
 }
 
+// Operação RETRIEVE (all), função listar()
+controller.listar = async (req, res) => {
+    try {
+        let dados = await Curso.find() // Traz todos os cursos cadastrados
+        res.send(dados) // Vai com status HTTP 200: OK
+    }
+    catch(erro) {
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+}
+
+// Operação RETRIEVE (one), função obterUm()
+controller.obterUm = async (req, res) => {
+    try {
+        // Capturando o parâmetro id da URL
+        const id = req.params.id
+        let obj = await Curso.findById(id)
+
+        // O objeto existe e foi encontrado
+        if(obj) res.send(obj)       // HTTP 200
+        // Não encontrado
+        else res.status(404).end()  // HTTP 404: Not found
+    }
+    catch(erro) {
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+}
+
 module.exports = controller
