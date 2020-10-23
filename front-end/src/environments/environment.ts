0ca56b5d629f6143273e5ab66cce8a35f7072337
fcintra4 <github4@faustocintra.com.br>
2020-10-23 01:28:01 +0000

    (22/10) Término da UI e criação do primeiro serviço

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiServer: 'https://3000-f471c076-e7e6-4c8e-b7df-1b942b927b6b.ws-us02.gitpod.io/'
 };
 
 /*
